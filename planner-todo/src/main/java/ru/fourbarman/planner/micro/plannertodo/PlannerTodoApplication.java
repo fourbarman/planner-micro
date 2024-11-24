df855b58f3c71aa2fbf93fee1134e129dbfe9f49
fourbarman <maks.java@yandex.ru>
2024-11-24 22:08:38 +0400

    RefreshScope test for update properties runtime with actuator.

--- planner-todo/src/main/java/ru/fourbarman/planner/micro/plannertodo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/ru/fourbarman/planner/micro/plannertodo/PlannerTodoApplication.java
@@ -3,6 +3,7 @@ package ru.fourbarman.planner.micro.plannertodo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "ru.fourbarman.planner.micro.plannertodo.repository")
 @EntityScan("ru.fourbarman.planner.micro.plannerentity.entity")
 @EnableFeignClients
+@RefreshScope
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
--- planner-users/src/main/java/ru/fourbarman/planner/micro/plannerusers/PlannerUsersApplication.java
+++ planner-users/src/main/java/ru/fourbarman/planner/micro/plannerusers/PlannerUsersApplication.java
@@ -3,6 +3,7 @@ package ru.fourbarman.planner.micro.plannerusers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages =
         "ru.fourbarman.planner.micro.plannerusers.repository")
 @EntityScan("ru.fourbarman.planner.micro.plannerentity.entity")
+@RefreshScope
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
